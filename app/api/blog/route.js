f3915a66d1fb7ada594b390797d2b88ea75053b7
Manish <man8872pal@gmail.com>
2025-07-22 19:19:36 +0530

    Blog page setup with backend

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -10,12 +10,22 @@ const LoadDB = async () => {
 
 LoadDB();
 
-
+//api endpoint to get all blogs
 export async function GET(request) {
-    console.log("Blog get hit");
-    return NextResponse.json({msg: "API working"})
+    const blogId = request.nextUrl.searchParams.get("id")
+
+    if(blogId) {
+        const blog = await BlogModel.findById(blogId)
+        return NextResponse.json(blog)
+    }
+    else {
+        const blogs = await BlogModel.find({});
+    return NextResponse.json({blogs})
+    }
 }
 
+
+//API endpoint for uploading blogs
 export async function POST(request) {
     const formData = await request.formData();
     const timeStamp = Date.now();
@@ -37,6 +47,7 @@ export async function POST(request) {
         authorImg: `${formData.get('authorImg')}`
     }
 
+    
     await BlogModel.create(blogData)
     console.log("Blog saved")
 
